ebdb2ef4d05eb8ffbaecfdba9b0d9342d6ff8dd8
JaiminR28 <jaiminrathwa28@gmail.com>
2023-05-19 11:09:37 +0530

    Adding GET and POST request for main page

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+const fs = require('fs');
 const express = require('express')
+const {dirname} = require('path')
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//         res.status(200).json({message:'Hello from the server side', app: 'Natours'})
-// })
+app.use(express.json())
 
-app.get('/api/tours')
+// app.get('/', (req, res) => {
+        //         res.status(200).json({message:'Hello from the server side', app: 'Natours'})
+        // })
+        
+const _dirname = dirname(__filename);
+const tours = JSON.parse( fs.readFileSync(`${_dirname}/dev-data/data/tours-simple.json`)) 
+
+app.get('/api/v1/tours', (req, res) => {
+        res.status(200).json({
+                status: 'success',
+                results: tours.length,
+                data: {
+                        tours
+                }
+        })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+        // console.log(req.body);
+        const newID = tours[tours.length - 1].id + 1;
+        const newTour = Object.assign({id: newID}, req.body)
+
+        tours.push(newTour);
+
+
+        fs.writeFile(`${_dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+                res.status(201).json({
+                        status: "success",
+                        data: {
+                                tour: newTour
+                        }
+                })
+        })
+})
 
 const PORT  = 3000;
 app.listen(PORT, () => {
