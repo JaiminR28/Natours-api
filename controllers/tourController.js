4094a6dcef1d6f7c0b8582f464fd31a053f75a5b
JaiminR28 <jaiminrathwa28@gmail.com>
2023-05-25 17:05:20 +0530

    Refactoring the APIFeatures into class and adding Aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,40 +1,28 @@
 // const { dirname } = require('path');
 
 const Tour = require('../models/tourModel');
+const APIFeatures = require('./utils/apiFeatures');
 
-// const _dirname = dirname(__filename);
-// const tours = JSON.parse(
-//   fs.readFileSync(`${_dirname}/../dev-data/data/tours-simple.json`)
-// );
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingAverage,price';
+  req.query.fields = 'name, price,ratingAverage,summary, difficulty';
 
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`tour id is:${val}`);
-//   // if (req.params.id * 1 > tours.length) {
-//   //   return res.status(404).json({
-//   //     status: 'fail',
-//   //     message: 'Invalid ID',
-//   //   });
-//   // }
-//   next();
-// };
-
-// exports.chechBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or price',
-//     });
-//     next();
-//   }
-//   //  Check if the body contains the name and price property
-//   //  If not, send back 400 ( bad request)
-//   //  Add it to the post handler stack
-//   next();
-// };
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
+
+    const tours = await features.query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -120,3 +108,120 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          num: { $sum: 1 },
+          numRating: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: {
+      //     _id: { $ne: 'easy' },
+      //   },
+      // },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: stats,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 6,
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      plan,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+// const _dirname = dirname(__filename);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${_dirname}/../dev-data/data/tours-simple.json`)
+// );
+
+// exports.checkID = (req, res, next, val) => {
+//   console.log(`tour id is:${val}`);
+//   // if (req.params.id * 1 > tours.length) {
+//   //   return res.status(404).json({
+//   //     status: 'fail',
+//   //     message: 'Invalid ID',
+//   //   });
+//   // }
+//   next();
+// };
+
+// exports.chechBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Missing name or price',
+//     });
+//     next();
+//   }
+//   //  Check if the body contains the name and price property
+//   //  If not, send back 400 ( bad request)
+//   //  Add it to the post handler stack
+//   next();
+// };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,14 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 //Create a checkBodymiddleware
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
+router.route('/montly-Plan/:year').get(tourController.getMonthlyPlan);
+
+router
+  .route('/top-5-tours')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
 router.route('/').get(tourController.getAllTours).post(tourController.addTour);
 router
   .route('/:id')
