af89ffeb808f8f3c9cb45246ad4b6d594152ceda
JaiminR28 <jaiminrathwa28@gmail.com>
2023-06-08 16:37:23 +0530

    creating geospatial queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -146,26 +146,44 @@ exports.getTourWithin = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getDistances(
-  catchAsync(async (req, res, next) => {
-    const { latlng, unit } = req.params;
-    const [lat, lng] = latlng.split(',');
-
-    if (!lat || !lng) {
-      return next(
-        new AppError(
-          'Please provide latitiude and longitude in the format lat, lng,',
-          400
-        )
-      );
-    }
-    const distances = await Tour.aggregate([
-      {
-        $geoNear: {},
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitiude and longitude in the format lat, lng,',
+        400
+      )
+    );
+  }
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
       },
-    ]);
-  })
-);
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
+});
 
 // const _dirname = dirname(__filename);
 // const tours = JSON.parse(
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -26,7 +26,7 @@ router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getTourWithin);
 
-router.route('/distances/"latlng/unit/:unit').get(tourController.getDistances);
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 router
   .route('/')
   .get(tourController.getAllTours)
