10dc829630d4e2c5578807cad44554d9a94308ff
JaiminR28 <jaiminrathwa28@gmail.com>
2023-08-25 23:57:03 +0530

    bug fix on login

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(express.json({ limit: '10kb' }));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
+  // console.log(req.cookies);
   next();
 });
 
